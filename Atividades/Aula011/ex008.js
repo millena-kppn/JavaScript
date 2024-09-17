36224d679b9b0ae40eb637728922b284181be8c5
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2024-09-17 15:17:15 -0300

    praticando .js

---
+++ Atividades/Aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`) //No JS console.log serve para valores de textos(strings)
+if (vel > 60){ //Condição simples!
+   var multado = console.log('Você ultrapassou  a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
+
---
+++ Atividades/Aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'Brasi'){
+   console.log('Você é Estrangeiro')
+}else{
+   console.log('Você é Brasileiro')
+}
