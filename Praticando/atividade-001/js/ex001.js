af5c3fa956855697a992fdcafac1420350444a06
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2024-10-27 11:39:13 -0300

    create js ex001 praticando

---
+++ Praticando/atividade-001/js/ex001.js
@@ -0,0 +1,30 @@
+// arquivo js externo
+
+document.write('Olá mundo!')
+console.log('Outra mensagem!')
+// alert('Olá mundo com Alert!')
+
+let mensagem = "Minha mensagem"
+let mensagem2 = 'Outra mensagem'
+let meuPeso = 51.5
+let minhaAltura = 1.68
+let minhaIdade = 20
+let ehDoador = false
+let teste = null
+
+// Criar Contantes e atribuir valor
+const PI = 3.1445
+const TAXA =0.5
+
+teste = 'Algum conteúdo'
+
+// Escrever o conteúdo de Variaveis e Constantes 
+document.write("<p> Mensagem: " + mensagem + "</p>")
+document.write(`Mensagem2: ${mensagem2}`)
+document.write("<p>Peso: " + meuPeso + " Kg </p>")
+document.write(`Altura: ${minhaAltura} m`)
+document.write("<p>Idade: " + minhaIdade + " anos </p>")
+document.write(`Doador? ${ehDoador}`)
+document.write("<p>Teste: " + teste + "</p>")
+document.write("<p>PI" + PI + "</p>")
+document.write("<p>TAXA" + TAXA + "</p>")
