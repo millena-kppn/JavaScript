d9a5f89f576a54943194351f4c0eef69b096a56e
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2024-10-30 15:00:20 -0300

    create arrays test

---
+++ Praticando/Array/js/test.js
@@ -0,0 +1,12 @@
+let livros = ['Mentirosos', 'Joyland', '1984']
+// REMOVER ARRAY NO FINAL (POP)
+livros.pop()
+var nomes = ['Ana', 'Paulo', 'Luis']
+// ADICIONAR NO INICIO (UNSHIFT)
+nomes.unshift('Alexandre', 'Laysa', 'Alice')
+// REMOVER NO INICIO (SHIFT)
+nomes.shift()
+// REMOVER DE UMA POSIÇÃO ESPECIFICA (SPLICE)
+nomes.splice(0,2)
+// COPIAR ARRAY E SELECIONAR QUANTIDADE DE COPIA (SLICE)
+let nome_f = nomes.slice(0,1)
