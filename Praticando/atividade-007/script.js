15e308d63e2cd075d59529d29e625a1ce6c5fe9d
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-01-15 19:35:22 -0300

    exercitando 007

---
+++ Praticando/atividade-007/script.js
@@ -0,0 +1,17 @@
+// Desafio 01
+let lutador = 'Fedor Vladimirovich Emelianenko'
+let nacionalidade = 'Russo'
+let idade = 44
+let peso = 106
+let altura = 1.83
+// para aparecer no navegador com ...
+document.write(`<div class="card">
+   <h2>Lutador</h2>
+   <img src="./IMGS/fedor-2015.jpg">
+   <p>${lutador}</p>
+   <p>${nacionalidade}</p>
+   <p>${idade} anos</p>
+   <p>${peso} Kg</p>
+   <p>${altura} m
+   </div></p>
+   `)
