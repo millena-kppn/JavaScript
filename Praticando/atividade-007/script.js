f384ef92e4e5d22d6bdda1954ecb25f76731b582
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-01-16 22:27:21 -0300

    desafio01 refeito com sintaxe

--- Praticando/atividade-007/script.js
+++ Praticando/atividade-007/script.js
@@ -4,14 +4,10 @@ let nacionalidade = 'Russo'
 let idade = 44
 let peso = 106
 let altura = 1.83
-// para aparecer no navegador com ...
-document.write(`<div class="card">
-   <h2>Lutador</h2>
-   <img src="./IMGS/fedor-2015.jpg">
-   <p>${lutador}</p>
-   <p>${nacionalidade}</p>
-   <p>${idade} anos</p>
-   <p>${peso} Kg</p>
-   <p>${altura} m
-   </div></p>
-   `)
+//para aparecer no navegador usamos ...
+document.getElementById("lutador").innerHTML = `${lutador}`
+document.getElementById("nacionalidade").innerHTML = `${nacionalidade}`
+document.getElementById("idade").innerHTML = `${idade} anos`
+document.getElementById("peso").innerHTML = `${peso} Kg`
+document.getElementById("altura").innerHTML = `${altura} m`
+
