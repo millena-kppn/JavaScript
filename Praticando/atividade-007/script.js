9198122e0228180938db3876a8b005ac7cce5432
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-01-17 21:34:47 -0300

    atvd 007 desafio 02

--- Praticando/atividade-007/script.js
+++ Praticando/atividade-007/script.js
@@ -10,4 +10,14 @@ document.getElementById("nacionalidade").innerHTML = `${nacionalidade}`
 document.getElementById("idade").innerHTML = `${idade} anos`
 document.getElementById("peso").innerHTML = `${peso} Kg`
 document.getElementById("altura").innerHTML = `${altura} m`
+// Desafio 02
+const dia = 10
+const mes = 9
+const ano = 2021
+const texto = "Seja curioso. Leia de tudo. Tente coisas novas. O que as pessoas chamam de inteligência se resume a curiosidade."
+const autor = "Aaron Swartz"
+//para aparecer no navegador usamos ...
+document.getElementById("data").innerHTML = `${dia}/${mes}/${ano}`
+document.getElementById("texto").innerHTML = `"${texto}"`
+document.getElementById("autor").innerHTML = `${autor}`
 
