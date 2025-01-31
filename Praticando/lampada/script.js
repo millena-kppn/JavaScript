ff149e76ea800378e54c00c48aa2c43d14307468
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-01-30 21:38:31 -0300

    the end

--- Praticando/lampada/script.js
+++ Praticando/lampada/script.js
@@ -1,1 +1,18 @@
-// continuar...
+let conteudo = document.querySelector('#conteudo')
+// no conteudo não há função
+let neutra = document.querySelector('#neutra')
+let on = document.querySelector('#on')
+let off = document.querySelector('#off')
+// criando funções
+on.addEventListener('click', ligar)
+off.addEventListener('click', desligar)
+function ligar(){
+   conteudo.classList.add('ligar')
+   conteudo.classList.remove('desligar')
+   neutra.style.opacity = 0
+}
+function desligar(){
+   conteudo.classList.add('desligar')
+   conteudo.classList.remove('ligar')
+   neutra.style.opacity = 0
+}
