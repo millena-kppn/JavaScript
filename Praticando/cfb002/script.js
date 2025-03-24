510094a24b3d7f2245b2644a663878ce0cbf574c
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-03-24 09:51:18 -0300

    create use strict

---
+++ Praticando/cfb002/script.js
@@ -0,0 +1,5 @@
+"use strict" //modo estrito = não irei conseguir usar a minha variável que não estiver declarada 
+let nome = "Mille"
+console.log(nome)// Irá imprimir a variável
+console.log("Meu nome")// Irá imprimir uma string
+console.log("Nome: " + nome)// Irá imprimir a string + variável com a concatenação
