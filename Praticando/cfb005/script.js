2aae601bd70484840d884b4f1331eee991873e5c
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-04-02 08:46:58 -0300

    operadores relacionais

---
+++ Praticando/cfb005/script.js
@@ -0,0 +1,10 @@
+"use strict"
+//OPEADORES RELACIONAIS = COMPARAÇÃO
+let num1 = 10, num2 = 5, num3 = 10
+console.log(num1 > num2) 
+console.log(num1 < num2)
+console.log(num1 >= num3)
+console.log(num1 == num3)
+console.log(!(num1 == num3)) // se le: Não é identico
+console.log(num1 != num3) // se le: É diferente 
+console.log(!(num1 != num3)) // se le: Não é diferente
