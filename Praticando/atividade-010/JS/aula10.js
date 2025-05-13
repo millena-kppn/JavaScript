80deada7f90d1f1132720dea3901f720615d31d8
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-05-13 10:32:31 -0300

    entendendo mais sobre function

--- Praticando/atividade-010/JS/aula10.js
+++ Praticando/atividade-010/JS/aula10.js
@@ -17,5 +17,40 @@ function olaMundo2 (){
    return 'Olá Mundo com retorno<br>'
 }
 // CHAMAR A FUNCAO = executar ela 
-olaMundo()
-document.write(olaMundo2())
+olaMundo()//fechamento da função 
+document.write(olaMundo2())//fechamento da função e colocando a informação de escrita na tela 
+
+let x = 2
+let y = 1
+let mensagem = "A pratica leva a perfeição!"
+
+//Funcoes com parametros e sem retorno 
+// (precedure)
+function somar(a, b){
+   document.write((a + b) + "<br>")
+}
+
+//Funcoes com paremetros e com retorno 
+// (function)
+function somar2(c, d){
+   return c + d
+}
+//CHAMAR A FUNCAO = executar ela 
+somar(x, y)
+document.write(somar2(10, 5))
+
+//Funcao anonima:
+//nao precisa usar a palavra function 
+//nao precisa usar a palavra return 
+//nao precisa colocar { } = cahaves se for so uma instrusao - sequencia de comandos 
+
+let button = document.getElementById("button")
+button.addEventListener('click' , function
+   () {
+      console.log("Você clicou no botão! Nele há uma função anonima.")
+   })
+
+//Arrow function 
+const olaMundoArrow = () => console.log("Olá mundo! Teste com Arrow function.")
+//Acima criei uma função anonima diretamente com uma variavel 
+olaMundoArrow()//execução dela 
